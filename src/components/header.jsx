bed3f1f6ed0750d83bf6cf0d1674686e8f6b6065
devHectorGa <Hetikor@outlook.com>
2018-12-30 12:00:00 -0500

    Agregando diseño inicial al Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './header.scss'
 
-const Header = () =>(
+const Header = (...props) =>(
   <header className="header">
-    <a href="void:javascript()" className="logo"><img src="imagotipo.svg" alt="imagotipo devHectorGa"/></a>
+    <a href="./" className="logo"><img src="imagotipo.svg" alt="imagotipo devHectorGa"/></a>
     <button className="nav_toggle"><i className="fas fa-bars fa-2x"></i></button>
     <nav className="header_nav">
       <ul className="nav">
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">Home</a></li>
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">About Me</a></li>
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">My Skills</a></li>
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">My Services</a></li>
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">My Projects</a></li>
-        <li className="nav_list"><a href="void:javascript()" className="nav_link">Contact Me</a></li>
+        <li className="nav_list"><a href="#home" className="nav_link">Home</a></li>
+        <li className="nav_list"><a href="#about" className="nav_link">About Me</a></li>
+        <li className="nav_list"><a href="#skills" className="nav_link">My Skills</a></li>
+        <li className="nav_list"><a href="#services" className="nav_link">My Services</a></li>
+        <li className="nav_list"><a href="#projects" className="nav_link">My Projects</a></li>
+        <li className="nav_list"><a href="#contact" className="nav_link">Contact Me</a></li>
       </ul>
     </nav>
   </header>
