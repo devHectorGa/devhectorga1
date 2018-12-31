ee8f645c4fcfac5aad5aea2188a015b5c373795e
devHectorGa <Hetikor@outlook.com>
2018-12-30 20:06:54 -0500

    Agregando estilos al componente About

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import './about.scss'
 
 const About = (...props) => (
   <section id="about" className="about">
-    <h2>About Me</h2>
-    <div className="slidePictures"></div>
+    <h2 className="about_title">About Me</h2>
+    <img className="about_picture" src="./imagotipo.svg" alt="Imagotipo devHector.Ga"/>
     <div className="about_data">
-      <div className="about_name">
+      <div className="data">
         <h3>Name</h3>
         <p>Hector Garzon</p>
       </div>
-      <div className="about_sex">
+      <div className="data">
         <h3>Sex</h3>
         <p>Male</p>
       </div>
-      <div className="about_address">
+      <div className="data">
         <h3>Address</h3>
         <p>Bogotá DC, Colombia</p>
       </div>
-      <div className="about_phone">
+      <div className="data">
         <h3>Phone Number</h3>
         <p>+57 310 123 4567</p>
       </div>
-      <div className="about_email">
+      <div className="data">
         <h3>Email</h3>
         <p><a href="mailto:contact@devHector.Ga">contacto@devHector.Ga</a></p>
       </div>
