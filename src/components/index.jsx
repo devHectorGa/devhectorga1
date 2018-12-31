752254cc6a8002519ed520a1c0d36418d012ea3b
devHectorGa <Hetikor@outlook.com>
2018-12-30 19:01:53 -0500

    Agregando componente About

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Header from './header'
 import Home from './home'
+import About from './about'
 // import 
 
 class App extends Component {
@@ -18,32 +19,7 @@ class App extends Component {
       <main>
         <Header />
         <Home />        
-        <section id="about" className="about">
-          <h2>About Me</h2>
-          <div className="slidePictures"></div>
-          <div className="about_data">
-            <div className="about_name">
-              <h3>Name</h3>
-              <p>Hector Garzon</p>
-            </div>
-            <div className="about_sex">
-              <h3>Sex</h3>
-              <p>Male</p>
-            </div>
-            <div className="about_address">
-              <h3>Address</h3>
-              <p>Bogotá DC, Colombia</p>
-            </div>
-            <div className="about_phone">
-              <h3>Phone Number</h3>
-              <p>+57 310 123 4567</p>
-            </div>
-            <div className="about_email">
-              <h3>Email</h3>
-              <p><a href="mailto:contact@devHector.Ga">contacto@devHector.Ga</a></p>
-            </div>
-          </div>
-        </section>
+        <About />
         <section id="skills" className="skills">
           <h2>My Skills</h2>
           <div className="skill">
