198ba74b0607c991c8ee05497dfd688dfda3f579
devHectorGa <Hetikor@outlook.com>
2019-01-15 16:43:18 -0500

    Agregando estilos al componente Services

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import './services.scss';
 
 const Services = (...props) => (
   <section id="services" className="services">
     <h2>My Services</h2>
-    <a href="#web" className="service">Web Design</a>
-    <a href="#creative" className="service">Creative Design</a>
-    <a href="#coding" className="service">Coding</a>
+    <a href="#web" className="service">
+      <i className="fab fa-react"></i>
+      Web Design
+    </a>
+    <a href="#creative" className="service">
+      <i className="fab fa-react"></i>
+      Creative Design
+    </a>
+    <a href="#coding" className="service">
+      <i className="fab fa-react"></i>
+      Coding
+    </a>
   </section>
 );
 
