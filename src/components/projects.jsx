f98959744b3819faf6e9c6a45a87f49554eaf4b4
devHectorGa <Hetikor@outlook.com>
2019-01-22 15:16:51 -0500

    Agregando estilos al componente Projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
 import './projects.scss';
+import image1 from './img/project01.jpg';
+import image2 from './img/project02.jpg';
 
 const Projects = (...props) => (
   <section id="projects" className="projects">
     <h2>My Projects</h2>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image1} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image2} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image1} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image2} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image1} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image2} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image1} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div className="project">
-      <img src="" alt="" />
-      <h3>Lorem Ipsum</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src={image2} alt="" className="projectImg" />
+      <h3 className="projectTitle">Lorem Ipsum</h3>
+      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
   </section>
 );
