38bd8debdbf1ae201545de328819cbe16b38fe50
devHectorGa <Hetikor@outlook.com>
2019-07-26 06:00:57 -0500

    Add firebase functions

--- src/components/sections/projects.jsx
+++ src/components/sections/projects.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import './css/projects.scss';
-import image1 from '../img/project01.jpg';
-import image2 from '../img/project02.jpg';
 
 const Projects = (...props) => (
   <section id="projects" className="projects">
     <h2>My Projects</h2>
     <div className="project">
-      <img src={image1} alt="" className="projectImg" />
-      <h3 className="projectTitle">Lorem Ipsum</h3>
-      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src="https://firebasestorage.googleapis.com/v0/b/devhectorga.appspot.com/o/projects%2Feventosylogistica.jpg?alt=media&token=9ccde24b-f65c-439b-8253-d4dfc5523305" alt="Eventos y Logistica Project devHector.Ga" className="projectImg" />
+      <h3 className="projectTitle">Eventos y Logistica website</h3>
+      <p className="projectText">A simple website for a local enterprise of events, designed with CSS, React and Firebase.</p>
       <span className="projectLinks">        
-        <a href="https://github.com/devHectorGa/devhectorga" target="_blank" className="projectLinks-link" rel="noopener noreferrer">
-          View more <i class="fab fa-github"></i>
+        <a href="https://eventosylogistica.net/" target="_blank" className="projectLinks-link" rel="noopener noreferrer">
+          <i class="fas fa-globe-americas "></i> Visit site
         </a>
       </span>
     </div>        
     <div className="project">
-      <img src={image2} alt="" className="projectImg" />
-      <h3 className="projectTitle">Lorem Ipsum</h3>
-      <p className="projectText">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <img src="https://firebasestorage.googleapis.com/v0/b/devhectorga.appspot.com/o/projects%2Fnacionaldemontacargas.jpg?alt=media&token=39bfffa8-1607-4344-bf48-da73e9e6b0d2" alt="Nacional de Montacargas Project devHector.Ga" className="projectImg" />
+      <h3 className="projectTitle">Nacional de Montacargas App</h3>
+      <p className="projectText">A complete website with user role, a protected section and real-time. Quotations and invoices (In development) and more features for clients.</p>
       <span className="projectLinks">        
-        <a href="https://github.com/devHectorGa/devhectorga" target="_blank" className="projectLinks-link" rel="noopener noreferrer">
-          View more <i class="fab fa-github"></i>
+        <a href="https://www.nacionaldemontacargas.com.co/" target="_blank" className="projectLinks-link" rel="noopener noreferrer">
+          <i class="fas fa-globe-americas "></i> Visit site
         </a>
       </span>
     </div>        
