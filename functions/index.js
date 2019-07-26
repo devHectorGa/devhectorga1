20d6a381912ff11f61fdd27ed92057b9977102fa
devHectorGa <Hetikor@outlook.com>
2019-07-26 09:57:16 -0500

    Signup function

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,26 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyDe-8xVkrc75YepY4vZ0GtdqNQFkOgAcXY",
+  authDomain: "devhectorga.firebaseapp.com",
+  databaseURL: "https://devhectorga.firebaseio.com",
+  projectId: "devhectorga",
+  storageBucket: "devhectorga.appspot.com",
+  messagingSenderId: "701704682698",
+  appId: "1:701704682698:web:fbfacf968344fb4c"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-  admin.firestore()
+  db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -32,13 +45,57 @@ app.post('/scream', (req,res) => {
     userHandle: req.body.userHandle,
     createdAt: new Date().toISOString()
   };
-  admin.firestore().collection('screams').add(newScream)
+  db.collection('screams').add(newScream)
     .then(doc => {
       res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch(err=>{
       res.status(500).json({error: "Someting went wrong"});
       console.error(err);
+    });
+});
+
+// Signup route
+app.post('/signup', (req,res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`).get().then(doc => {
+    if(doc.exists){
+      return res.status(400).json({ handle: 'This handle is already taken' });
+    }else{
+      return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+    }
+  })
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then( () => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if(err.code === 'auth/email.already-in-use'){
+        return res.status(400).json({ email: 'Email is already in use' });
+      }else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -8,7 +8,6 @@ class App extends Component {
     this.state = {
     };
   }
-  
   render() {
     return (
       // <Sections />
